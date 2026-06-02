35bd7f18cad40f084d1a765a88f80761a81be4f8
Luciano Peralta <user_ext_luperalta>
2026-06-02 19:44:11 -0300

    chore: upgrade project to .NET 10.0 and update package references

--- GymAdmin/src/GymAdmin.Api/Program.cs
+++ GymAdmin/src/GymAdmin.Api/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // ===== HOST =====
